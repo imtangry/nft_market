f8b5236a950d00a26afa67d49ea5acb7e1e76b8a
不知名的底层码农T先生 <private_1631024@163.com>
2024-04-22 14:59:33 +0800

    feat: Title组件添加button

--- components/Shared/Title.js
+++ components/Shared/Title.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function Title({ children, title = '', subTitle = '' }) {
+function Title({ children, title = '', subTitle = '', button }) {
     return (
-        <div className='text-slate-900 dark:text-slate-50 my-14 px-4'>
-            <h1 className='text-3xl font-bold mb-2'>{title || children}</h1>
-            {subTitle && <p>{subTitle}</p>}
+        <div className='text-slate-900 dark:text-slate-50 my-14 px-4 flex items-center justify-between'>
+            <div>
+                <h1 className='text-3xl font-bold mb-2'>{title || children}</h1>
+                {subTitle && <p>{subTitle}</p>}
+            </div>
+            <div className='text-lg'>
+                {button}
+            </div>
         </div>
     )
 }
